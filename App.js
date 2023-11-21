c90fd230478aff4e83e823bd2f915301927ae10f
Mohit R Chugh <mohitrchugh@Mohits-Air.Dlink>
2023-11-22 01:55:49 +0530

    Added a few things

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+//Using React createElement
+// const heading = React.createElement("h1", {id: "heading"}, "This is a heading");
+// console.log(heading);
+//Using JSX creation
+const JSXHeading = <h1 id="heading">React is AMAZING!</h1>
+console.log(JSXHeading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+root.render(JSXHeading);
+
+
+
 /**
  * <div id="parent">
  *      <div id="child">
@@ -17,41 +29,41 @@ import ReactDOM from 'react-dom/client';
 //createElement
 //create nested elements
 //use of root render
-const parent = React.createElement(
-    "div",
-    {id: "parent"},//attribute
-    [React.createElement(
-        "div",
-        {id: "child"},
-        [React.createElement(
-            "h1",
-            {},
-            "React is Amazing"
-        ),
-        React.createElement(
-            "h2",
-            {},
-            "I am a h2 Tag"
-        )
-        ]
-      ),
-      React.createElement(
-        "div",
-        {id: "child2"},
-        [React.createElement(
-            "h1",
-            {},
-            "I am a h1 Tag"
-        ),
-        React.createElement(
-            "h2",
-            {},
-            "I am a h2 Tag"
-        )
-        ]
-      )
-    ]//Children
-    ); //It is a JS object
-    console.log(parent); 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// const parent = React.createElement(
+//     "div",
+//     {id: "parent"},//attribute
+//     [React.createElement(
+//         "div",
+//         {id: "child"},
+//         [React.createElement(
+//             "h1",
+//             {},
+//             "React is Amazing"
+//         ),
+//         React.createElement(
+//             "h2",
+//             {},
+//             "I am a h2 Tag"
+//         )
+//         ]
+//       ),
+//       React.createElement(
+//         "div",
+//         {id: "child2"},
+//         [React.createElement(
+//             "h1",
+//             {},
+//             "I am a h1 Tag"
+//         ),
+//         React.createElement(
+//             "h2",
+//             {},
+//             "I am a h2 Tag"
+//         )
+//         ]
+//       )
+//     ]//Children
+//     ); //It is a JS object
+//     console.log(parent); 
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
